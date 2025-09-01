9fd351e94a8ac8fd1371099981261f67d9c06bf1
David Avendano <d.avendano.c@hotmail.com>
2025-09-01 12:38:58 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# SwiftUICentered
+
+## Installation
+
+To install this package, import `url here` in SPM.
+
+## Usage example
+
+```swift
+
+struct ContentView: View {
+    var body: Some View {
+        VStack {
+            Text("Centered")
+            .centerHorizontally()
+        }
+    }
+}
+
+```
--- Sources/SwiftUICentered/SwiftUICentered.swift
+++ Sources/SwiftUICentered/SwiftUICentered.swift
@@ -1,2 +1,19 @@
+<<<<<<< HEAD
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+=======
+
+import SwiftUI
+
+extension View {
+    public func centerHorizontally() -> some View {
+        HStack {
+            Spacer()
+            
+            self
+            
+            Spacer()
+        }
+    }
+}
+>>>>>>> ae135dd (Initial Commit)
